3a19625a0322f10eef86207c002730c264ec426c
Niko Hienonen <niko.hienonen@gmail.com>
2019-03-12 18:10:07 +0200

    header&footer

---
+++ frontend/src/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/static/Header';
+import Footer from './components/static/Footer';
+
+class Main extends Component {
+  render() {
+    return(
+      <div>
+        <Header/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+export default Main;
---
+++ frontend/src/components/static/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component{
+    render(){
+        return(
+            <footer>
+                <p>Copyright: Hienonen & Tannerma, 2019</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
