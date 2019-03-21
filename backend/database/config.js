2165190ddbd0ce65175df11ccb062c2974773c20
Teemu Tannerma <teemu.tannerma@gmail.com>
2019-03-21 18:00:10 +0200

    Added backend connection.

---
+++ backend/database/config.js
@@ -0,0 +1,8 @@
+const config = {
+    host: 'mydb.tamk.fi',
+    user: 'c7ttanne',
+    password: 'Tietokanta1',
+    database: 'dbc7ttanne72'
+};
+
+module.exports = { config };
---
+++ backend/index.js
@@ -0,0 +1,36 @@
+'use strict';
+const express = require('express');
+const crudrepo = require('./database/crudrepository.js');
+const app = express();
+app.use(express.json());
+
+const server = app.listen(8080, () => {
+    console.log(`Listening on port ${server.address().port}`);
+});
+
+app.get('/products', function (req, res) {
+    crudrepo.getProducts((results) => {
+        res.send(results);
+    });
+});
+/*
+app.get('/products/:id([0-9]+)', (req, res) => {
+  crudrepo.findById(req.params.id, (results) => {
+    let obj = JSON.stringify(results)
+    res.send(`Results with id: ${req.params.id} ` + '\n' + obj)
+  })
+})
+
+app.post('/products', (req, res) => {
+  var productData = req.body
+  crudrepo.save(productData, (result) => {
+    res.send(result)
+  })
+})
+
+app.delete('/products/:id([0-9]+)', (req, res) => {
+  crudrepo.deleteById(req.params.id, (results) => {
+    res.send(results)
+  })
+})
+*/
