226e72135b927af35eea0c74defd916cf0b01a93
Teemu Tannerma <teemu.tannerma@gmail.com>
2019-03-31 11:38:34 +0300

    Added functionality to search bar

--- frontend/src/components/static/SearchBar.js
+++ frontend/src/components/static/SearchBar.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
+    constructor() {
+        super()
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.state = {
+            searchValue: ''
+        }
+    }
+    handleChange(event) {
+        this.setState({searchValue: event.target.value})
+    }
+    handleSubmit(event) {
+        console.log(this.state.searchValue)
+        event.preventDefault()
+    }
   render(){
     return(
-      <input type="text" placeholder="Search..."></input>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" onChange={this.handleChange} placeholder="Search..."></input>
+        <button name="Search nav-button" type="submit">Search</button>
+      </form>
     )
   }
 }
