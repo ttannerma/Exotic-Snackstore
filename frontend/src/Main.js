958bb841a4d9920bcb5d707c261061e4b1c5c88a
Niko Hienonen <niko.hienonen@gmail.com>
2019-04-03 11:17:34 +0300

    Router and Link added

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -3,16 +3,19 @@ import Header from './components/static/Header';
 import Footer from './components/static/Footer';
 import Navigation from './components/static/Navigation';
 import Body from './components/dynamic/Body';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class Main extends Component {
   render() {
     return(
+      <Router>
         <div className="container">
           <Header/>
           <Navigation/>
           <Body/>
           <Footer/>
         </div>
+      </Router>
     )
   }
 }
