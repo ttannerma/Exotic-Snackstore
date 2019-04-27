6b9c84bfdf0215bcb10c080b5044e4f86bcba272
Teemu Tannerma <teemu.tannerma@gmail.com>
2019-04-27 09:02:17 +0300

    Added comments

--- frontend/src/shoppingcart-context.js
+++ frontend/src/shoppingcart-context.js
@@ -4,26 +4,30 @@ export const ShoppingCartContext = React.createContext()
 
 export class ShoppingCartProvider extends Component {
 
+    // State contains product array which will contain product objects.
+    // count contains product count.
     state = {
         products: [],
         count: 0
     }
 
     removeItem = (id) => {
-        console.log('remove item state: ', this.state.products)
+        // Create state copy
         let stateCopy = this.state.products
-        // Iterate shopping cart products
+        // Iterate shopping cart products and search the index of item that is to be removed
         for(let i = 0; i < stateCopy.length; i++) {
-            // Check if new product exists already in cart.
             if(stateCopy[i].id === id) {
-                    let removeIndex = stateCopy.map((item) => { return item.id}).indexOf(id)
-                    stateCopy.splice(removeIndex, 1)
+                // Set the item's index by mapping it and then searching the index with indexof
+                let removeIndex = stateCopy.map((item) => { return item.id}).indexOf(id)
+                // Remove item object from state copy
+                stateCopy.splice(removeIndex, 1)
             }
         }
-        this.updateItemCount()
+
         this.setState({products: stateCopy, count: this.updateItemCount()})
     }
 
+    // Updates item count after setState
     updateItemCount = () => {
         let updatedItemCount = 0
         if(this.state.products.length > 0) {
@@ -35,11 +39,13 @@ export class ShoppingCartProvider extends Component {
         return updatedItemCount
     }
 
+    // Checks duplicate item entries to shopping cart
     checkDuplicateItems(newProduct) {
         let stateCopy = this.state.products
         let counter = 0;
         // Iterate shopping cart products
         for(let i = 0; i < stateCopy.length; i++) {
+            console.log(counter)
             // Check if new product exists already in cart.
             if(stateCopy[i].id === newProduct.id) {
                 counter++
@@ -49,24 +55,24 @@ export class ShoppingCartProvider extends Component {
                     let removeIndex = stateCopy.map((item) => { return item.id}).indexOf(newProduct.id)
                     newProduct.quantity += stateCopy[removeIndex].quantity
                     stateCopy.splice(removeIndex, 1)
+                    this.setState({ products: stateCopy, count: this.updateItemCount()})
                 }
             }
         }
     }
 
+    // Sets product data to shopping cart
     setProductId = (name, id, amount, price) => {
+        // Create state copy of products
         let allProducts = this.state.products
         let newProduct = {name: name, id: id, quantity: amount, price: price}
-        let productCount = 0;
         allProducts.push(newProduct)
-        this.setState({
-            products: allProducts
-        })
+        // If new product already exists on cart, then update quantity on existing product.
         this.checkDuplicateItems(newProduct)
-        for(let i = 0; i < allProducts.length; i++) {
-            productCount += allProducts[i].quantity
-        }
-        this.setState({ count: productCount})
+        // Set new state with new product and updated item count.
+        this.setState({
+            products: allProducts, count: this.updateItemCount()})
+        
     }
 
     render() {
