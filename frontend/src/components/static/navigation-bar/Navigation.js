6df6546539fb3c657ea55e939c43093395934c32
Niko Hienonen <niko.hienonen@gmail.com>
2019-05-07 12:10:39 +0300

    updates to previous

--- frontend/src/components/static/navigation-bar/Navigation.js
+++ frontend/src/components/static/navigation-bar/Navigation.js
@@ -12,10 +12,14 @@ class Navigation extends Component{
     this.state = {
       countries
       , categories
-      , isLoggedIn: sessionStorage.getItem("activeUserType")
+      , isLoggedIn: ''
     };
   }
+  componentWillMount () {
+    this.setState({isLoggedIn: sessionStorage.getItem("activeUserType")})
+  }
   componentDidMount = () => {
+    //this.setState({isLoggedIn: sessionStorage.getItem("activeUserType")})
     window.onclick = function(event) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       if (!event.target.matches('.dropbutton')) {
@@ -43,8 +47,7 @@ class Navigation extends Component{
     toggleTarget.classList.toggle('hidden');
   }
   LoginOrLogout = () => {
-    console.log(sessionStorage.getItem("activeUserType"))
-    if(sessionStorage.getItem("activeUserType")) {
+    if(this.state.isLoggedIn) {
       return <NavButton icon="fas fa-sign-out-alt" name="Logout"/>
     } else {
       return <NavButton icon="fas fa-sign-in-alt" name="Login"/>
